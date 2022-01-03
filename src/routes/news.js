9cf4fc056a70abcf248ca71211156f097e2a23ab
alexjoshuaboj <alejandrobojneag@gmail.com>
2022-01-03 13:31:54 +0100

    all end points

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const newsSchema = require('../models/news');
 
-//create new
+// create new
 router.post('/news', (req, res) => {
     const news = newsSchema(req.body);
     news.save()
@@ -21,4 +21,25 @@ router.get('/news', (req, res) => {
         ))
 })
 
+// delete new
+router.delete('/news/:id', (req, res) => {
+    const { id } = req.params;
+    newsSchema.remove({ _id : id })
+        .then((data) => res.json(data))
+        .catch((err) => res.json(
+            {msg: err}
+        ))
+})
+
+// archived new
+
+router.put('/news/:id', (req, res) => {
+    const { id } = req.params;
+    const { archived } = req.body;
+    newsSchema.updateOne({ _id : id }, {$set: { archived }})
+        .then((data) => res.json(data))
+        .catch((err) => res.json(
+            {msg: err}
+        ))
+})
 module.exports = router;
